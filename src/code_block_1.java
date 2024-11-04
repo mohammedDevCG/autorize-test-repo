f359a0a183951dff02d56d4f65cc496886942ff1
mohammedDevCG <mohammed-abbas.patrawala@capgemini.com>
2024-11-04 17:16:37 +0530

    automation

--- src/code_block_1.java
+++ src/code_block_1.java
@@ -1,104 +1,83 @@
-package com.example.pages;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-public class LoanApplicationPage {
-    WebDriver driver;
-
-    @FindBy(id = "userId")
-    WebElement userIdField;
-
-    @FindBy(id = "password")
-    WebElement passwordField;
-
-    @FindBy(id = "loginButton")
-    WebElement loginButton;
-
-    @FindBy(xpath = "//div[@id='docChecklist']//li[contains(@class, 'addressProof')]")
-    WebElement addressProof;
-
-    @FindBy(xpath = "//div[@id='docChecklist']//li[contains(@class, 'propertyProof')]")
-    WebElement propertyProof;
-
-    @FindBy(xpath = "//div[@id='docChecklist']//li[contains(@class, 'incomeProof')]")
-    WebElement incomeProof;
-
-    @FindBy(xpath = "//div[@id='docChecklist']//li[contains(@class, 'creditScoreDoc')]")
-    WebElement creditScoreDoc;
-
-    @FindBy(id = "verificationCheckbox")
-    WebElement verificationCheckbox;
-
-    public LoanApplicationPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
-
-    public void login(String userId, String password) {
-        userIdField.sendKeys(userId);
-        passwordField.sendKeys(password);
-        loginButton.click();
-    }
-
-    public void openDocument(WebElement document) {
-        document.click();
-    }
-
-    public void verifyDocument(WebElement document) {
-        document.click();
-        if (!document.getAttribute("class").contains("verified")) {
-            throw new AssertionError("Document verification failed");
-        }
-    }
-
-    public void completeVerification() {
-        verificationCheckbox.click();
-        if (!verificationCheckbox.getAttribute("class").contains("verified")) {
-            throw new AssertionError("Verification process completion failed");
-        }
-    }
-}
-
 package com.example.tests;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import com.example.pages.LoanApplicationPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import java.util.List;
 
-public class LoanApplicationTest {
-    WebDriver driver;
-    LoanApplicationPage loanApplicationPage;
+public class DocumentVerificationTests {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        loanApplicationPage = new LoanApplicationPage(driver);
-    }
-
-    @Test
-    public void testDocumentVerification() {
-        loanApplicationPage.login("user123", "abcabd");
-        loanApplicationPage.openDocument(loanApplicationPage.addressProof);
-        loanApplicationPage.verifyDocument(loanApplicationPage.addressProof);
-        loanApplicationPage.openDocument(loanApplicationPage.propertyProof);
-        loanApplicationPage.verifyDocument(loanApplicationPage.propertyProof);
-        loanApplicationPage.openDocument(loanApplicationPage.incomeProof);
-        loanApplicationPage.verifyDocument(loanApplicationPage.incomeProof);
-        loanApplicationPage.openDocument(loanApplicationPage.creditScoreDoc);
-        loanApplicationPage.verifyDocument(loanApplicationPage.creditScoreDoc);
-        loanApplicationPage.completeVerification();
+        // Setup code to initialize WebDriver, WebDriverWait
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @DataProvider(name = "documentChecklistData")
+    public Object[][] documentChecklistData() {
+        return new Object[][]{
+            {"user123", "abcabd", new String[]{"Address proof", "property completion state proof", "income proof", "credit score document"}}
+        };
+    }
+
+    @Test(dataProvider = "documentChecklistData")
+    public void testDocumentVerificationProcess(String username, String password, String[] documents) {
+        login(username, password);
+        navigateToLoanApplication();
+        verifyDocumentsPresent(documents);
+        openAndVerifyDocuments(documents);
+        completeVerification();
+    }
+
+    private void login(String username, String password) {
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("loginButton")).click();
+    }
+
+    private void navigateToLoanApplication() {
+        // Code to navigate to the loan application page
+    }
+
+    private void verifyDocumentsPresent(String[] documents) {
+        List<WebElement> documentElements = driver.findElements(By.xpath("//div[@class='document-list']/div[@class='document']"));
+        for (String document : documents) {
+            boolean isPresent = documentElements.stream().anyMatch(el -> el.getText().equals(document));
+            assert isPresent : "Document " + document + " is not present";
+        }
+    }
+
+    private void openAndVerifyDocuments(String[] documents) {
+        for (String document : documents) {
+            WebElement docElement = driver.findElement(By.xpath("//div[text()='" + document + "']"));
+            docElement.click(); // Simulating double click
+
+            WebElement checkbox = driver.findElement(By.xpath("//div[@class='checkbox'][@data-doc='" + document + "']"));
+            checkbox.click();
+            String color = checkbox.getCssValue("background-color");
+            assert color.equals("green") : "Checkbox for " + document + " did not turn green";
+        }
+    }
+
+    private void completeVerification() {
+        WebElement completeCheckbox = driver.findElement(By.id("completeVerification"));
+        completeCheckbox.click();
+        String color = completeCheckbox.getCssValue("background-color");
+        assert color.equals("green") : "Complete verification checkbox did not turn green";
     }
 }
