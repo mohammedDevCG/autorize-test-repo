50dbf19be79b23ea59295f6aa6381c889ee56567
mohammedDevCG <mohammed-abbas.patrawala@capgemini.com>
2024-11-07 15:43:41 +0530

    first

--- src/code_block_1.java
+++ src/code_block_1.java
@@ -1,24 +1,39 @@
 package com.example.tests;
 
-import org.openqa.selenium.By;
+import com.example.pages.LoginPage;
+import com.example.pages.CartPage;
+import com.example.pages.CheckoutPage;
+import com.example.pages.InventoryPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.util.List;
 
-public class DocumentVerificationTests {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+public class TestSuite {
     private WebDriver driver;
-    private WebDriverWait wait;
+    private Properties prop;
 
     @BeforeMethod
-    public void setUp() {
-        // Setup code to initialize WebDriver, WebDriverWait
+    public void setUp() throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+        prop.load(fis);
+
+        String browser = prop.getProperty("browser");
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }
+        // Additional browser configurations
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
@@ -28,56 +43,43 @@ public class DocumentVerificationTests {
         }
     }
 
-    @DataProvider(name = "documentChecklistData")
-    public Object[][] documentChecklistData() {
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
         return new Object[][]{
-            {"user123", "abcabd", new String[]{"Address proof", "property completion state proof", "income proof", "credit score document"}}
+                {"https://www.saucedemo.com/", "standard_user", "secret_sauce"}
         };
     }
 
-    @Test(dataProvider = "documentChecklistData")
-    public void testDocumentVerificationProcess(String username, String password, String[] documents) {
-        login(username, password);
-        navigateToLoanApplication();
-        verifyDocumentsPresent(documents);
-        openAndVerifyDocuments(documents);
-        completeVerification();
+    @Test(dataProvider = "loginData")
+    public void testLoginFunctionality(String url, String username, String password) {
+        driver.get(url);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
+        loginPage.clickLogin();
     }
 
-    private void login(String username, String password) {
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("loginButton")).click();
-    }
+    @Test(dataProvider = "loginData")
+    public void testShoppingFunctionality(String url, String username, String password) {
+        driver.get(url);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
+        loginPage.clickLogin();
 
-    private void navigateToLoanApplication() {
-        // Code to navigate to the loan application page
-    }
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.addItemToCart();
 
-    private void verifyDocumentsPresent(String[] documents) {
-        List<WebElement> documentElements = driver.findElements(By.xpath("//div[@class='document-list']/div[@class='document']"));
-        for (String document : documents) {
-            boolean isPresent = documentElements.stream().anyMatch(el -> el.getText().equals(document));
-            assert isPresent : "Document " + document + " is not present";
-        }
-    }
+        CartPage cartPage = new CartPage(driver);
+        cartPage.navigateToCart();
+        cartPage.verifyItemsInCart();
 
-    private void openAndVerifyDocuments(String[] documents) {
-        for (String document : documents) {
-            WebElement docElement = driver.findElement(By.xpath("//div[text()='" + document + "']"));
-            docElement.click(); // Simulating double click
-
-            WebElement checkbox = driver.findElement(By.xpath("//div[@class='checkbox'][@data-doc='" + document + "']"));
-            checkbox.click();
-            String color = checkbox.getCssValue("background-color");
-            assert color.equals("green") : "Checkbox for " + document + " did not turn green";
-        }
-    }
-
-    private void completeVerification() {
-        WebElement completeCheckbox = driver.findElement(By.id("completeVerification"));
-        completeCheckbox.click();
-        String color = completeCheckbox.getCssValue("background-color");
-        assert color.equals("green") : "Complete verification checkbox did not turn green";
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.clickCheckout();
+        checkoutPage.enterCheckoutInformation("John", "Doe", "12345");
+        checkoutPage.clickContinue();
+        checkoutPage.verifyTotalCost();
+        checkoutPage.clickFinish();
+        checkoutPage.verifyThankYouMessage();
     }
 }
